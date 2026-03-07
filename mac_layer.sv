0483aee2dc19db70140e5f34d9837eabc87f712c
omermumtaz05 <omermumtaz05@gmail.com>
2026-03-07 22:22:04 +0000

    Add mac_layer.sv

---
+++ mac_layer.sv
@@ -0,0 +1,18 @@
+// Code your design here
+module MAC(
+  input [31:0] Ain,
+  input [31:0] Bin,
+  input [31:0] Cin,
+
+  output [31:0] Aout,
+  output [31:0] Bout,
+  output [31:0] Cout
+);
+  
+  reg [31:0] regA, regB, regC; 
+  
+  
+  
+  
+  
+endmodule

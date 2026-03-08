f709669a5df633599eebba1ffa27f17a03c90401
omermumtaz05 <omermumtaz05@gmail.com>
2026-03-08 18:17:27 +0000

    Your commit message describing the changes

--- mac_layer.sv
+++ mac_layer.sv
@@ -1,17 +1,46 @@
-// Code your design here
-module MAC(
-  input [31:0] Ain,
-  input [31:0] Bin,
-  input [31:0] Cin,
 
-  output [31:0] Aout,
-  output [31:0] Bout,
-  output [31:0] Cout
+module MAC(
+  input reset,
+  input clock,
+  input logic [31:0] Ain,
+  input logic [31:0] Bin,
+  input logic [31:0] Cin,
+
+  output logic [31:0] Aout,
+  output logic [31:0] Bout,
+  output logic [31:0] Cout
 );
   
   reg [31:0] regA, regB, regC; 
   
   
+  always_comb
+
+    
+    begin
+      regA = Ain;
+      regB = Bin;
+      regC = Cin + (Ain * Bin);
+      
+    	
+    end
+  
+  	
+  always_ff @ (posedge clock)
+    if(reset)
+      begin
+        Cout <= '0;
+        Aout <= '0;
+        Bout <= '0;
+      end
+    else
+      begin
+        Cout <= regC;
+        Aout <= regA;
+        Bout <= regB;
+
+      end
+
   
   
   
